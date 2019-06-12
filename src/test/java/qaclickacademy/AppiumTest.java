3baa7f797c339640998fc4cd879a367a1a6c9490
Aditya Reddy <adityareddysingireddy@Adityas-MacBook-Pro.local>
2019-06-12 12:22:16 -0400

    first

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+
+	@Test
+	public void NavtiveAPpAndriod()
+	{
+		System.out.println("NavtiveAPpAndriod");
+	}
+	
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("iosapps");
+	}
+}
